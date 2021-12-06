e50a01dae9d16203d856e4053978cebf30139024
Cerceis <48720021+Cerceis@users.noreply.github.com>
2021-12-06 14:25:15 +0900

    Updated Readme and minir fixes

---
+++ GetRandomElement/README.md
@@ -0,0 +1,6 @@
+@Description
+    - Get random N numbers of element from the given array.
+    
+@Params
+    - arr: number<required>: Input array.
+    - noOfResult: number<default=1>: Number of random element return.
--- GetRandomElement/get-random-element.ts
+++ GetRandomElement/get-random-element.ts
@@ -1,7 +1,7 @@
-export const getRandomFromArray = (arr, noOfResult)=>{
-    if(!Array.isArray(arr)) throw "Input must be an array"
+export const getRandomFromArray = (arr: any[], noOfResult: number) => {
+    if (!Array.isArray(arr)) throw "Input must be an array"
     const result = []
-    for(let i = 0 ; i<noOfResult ; i++)
+    for (let i = 0; i < noOfResult; i++)
         result.push(arr[Math.floor(Math.random() * arr.length)]);
     return result
 }
